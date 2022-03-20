462ae8ab85a45ef0b7cdabe37fd0362158d5d1a0
AliHaidar97 <50525770+AliHaidar97@users.noreply.github.com>
2022-03-20 15:33:48 +0100

    Add files via upload

---
+++ PartA+B_Without_Spark_Streaming/src/main/java/inf583/wordcount_spark/source.java
@@ -0,0 +1,13 @@
+package inf583.wordcount_spark;
+
+import java.io.IOException;
+
+public class source {
+	
+	public static void main( String[] args) throws ClassNotFoundException, IOException, InterruptedException
+
+	{
+		
+
+	}
+}
